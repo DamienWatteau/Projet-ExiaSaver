4d27acf136a5865f6f24bf4bc13f94d03bb3fd64
DamienWatteau <damienwatteau@outlook.fr>
2016-12-14 10:03:09 +0100

    Update random.c

--- EXIASAVER_HOME/random.c
+++ EXIASAVER_HOME/random.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> // La bibliothéque contient "printf"
+#include <stdlib.h> // Elle contient fonction "srand" / "rand"
 
-int alea(int min, int max)
+int alea(int min, int max)                                   // Paramètres de la fonction qui ont été envoyés
 {
+   /*Déclaration des variables*/
     int nbgen = 0;
-    srand(time(NULL));
-    nbgen=(rand() % (max - min +1)) + min;
-    return nbgen;
+
+    srand(time(NULL));                                      // Initialise la fonction srand sur le temps actuel
+    nbgen=(rand() % (max - min +1)) + min;                  // Rand retourne un un nombre aléatoire dans la variable "nbgen" comprit entre "min" et "max"
+    return nbgen;                                           // Renvoie la valeur dans la variable "nbgen"
 }
 
