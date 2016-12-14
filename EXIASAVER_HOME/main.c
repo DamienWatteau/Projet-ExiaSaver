3e4867c34247cc524443e0bd643d67f0ff5ad090
DamienWatteau <damienwatteau@outlook.fr>
2016-12-14 09:21:24 +0100

    Ajout commentaires

--- EXIASAVER_HOME/main.c
+++ EXIASAVER_HOME/main.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h> // (Fork)
+#include <stdio.h>  // Cette bibliothèque renferme les routines de traitement d'entrée/sortie fichier.
+#include <stdlib.h> // Cette bibliothèque renferme les routines Standard.
+#include <sys/types.h>  // Cette bibliothèque renferme les définitions de temps pour Unix. 
+#include <sys/wait.h>   // Cette bibliothèque renferme les définitions d'attente.
+#include <unistd.h> //  Cette bibliothèque renferme les types et les constantes symbole standard.
 
 #include "random.h"
 #include "termSaver1.h"
@@ -18,10 +18,12 @@ int main(int argc, char *argv[])
     // while (1){
     pid_t pid = 0;
     int nbgen = 0;
-
+    
+    /* Détection du "-stats"*/
+    
     if (argc != 1 && strcmp(argv[1], "-stats") == 0)
     {
-        history();
+        history();          // Si détection d'un paramètre et 
     }
     else if (argc != 1 && strcmp(argv[1], "-stats") != 0)
     {
@@ -32,30 +34,31 @@ int main(int argc, char *argv[])
         nbgen = alea(1, 3);
     }
     
-    history(); //
-    return 0;
     system("clear");
+    
+    /* Choix dépendant d'une variable aléatoire (nbgen) permmettant de choisir le type de mode veille*/
     
     if (nbgen == 1)
     {
-        pid=fork();
-        system("clear");
-        if (pid == 0)
+        pid=fork();                                     // Il génére et stocke le PID du fils dans la variable "pid"
+        system("clear");                                // Efface le contenu de la console
+        if (pid == 0)                                
         {
-            termSaver1();
+            termSaver1();                             // Si on est dans le processus fils alors il lance le mode veille statique 
+            // Detection de la touche 
         }
         else
         {
-                    //Détection touche entrée
+           wait(NULL);                              // *                                      
         }
     }
     else if (nbgen == 2)
     {
-        pid=fork();
-        system("clear");
+        pid=fork();                                        // Il génére et stocke le PID du fils dans la variable "pid"
+        system("clear");                                   // Efface le contenu de la console
         if (pid == 0)
         {
-            termSaver2();
+            termSaver2();                                // Si on est dans le processus fils alors il lance le mode veille dynamique 
         }
         else
         {
@@ -64,11 +67,11 @@ int main(int argc, char *argv[])
         }
         else
         {
-            pid=fork();
-            system("clear");
+            pid=fork();                                  // Il génére et stocke le PID du fils dans la variable "pid"
+            system("clear");                            // Efface le contenu de la console
                 if (pid == 0)
                 {
-                    termSaver3();
+                    termSaver3();                       // Si on est dans le processus fils alors il lance le mode veille interactif 
                 }
                 else
                 {
