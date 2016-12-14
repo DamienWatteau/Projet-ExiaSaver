45ccfe6e9192a84f7dad5f3d00677b58e559a07e
DamienWatteau <damienwatteau@outlook.fr>
2016-12-14 10:50:45 +0100

    Update history_ecriture.c

--- EXIASAVER_HOME/history_ecriture.c
+++ EXIASAVER_HOME/history_ecriture.c
@@ -6,36 +6,35 @@ void history_ecriture (int *nbF, int mode)
 {
 
    /* lire l'heure courante */
-   time_t now = time (NULL);    /* now contient maintenant la date et l'heure courante */
+   time_t now = time (NULL);    // La variable "now" contient la date et l'heure courante
 
-   /* la convertir en heure locale */
-   struct tm tm_now = *localtime (&now);
+   struct tm tm_now = *localtime (&now); // Conversion en heure locale
 
-   /* Creer une chaine JJ/MM/AAAA HH:MM:SS */
-   char s_now[sizeof "JJ/MM/AAAA HH:MM:SS"];
+   char s_now[sizeof "JJ/MM/AAAA HH:MM:SS"]; // Creer une chaine JJ/MM/AAAA HH:MM:SS
 
    /* Formater la date et l'heure (Il enregistre la date et l'heure dans la chaine de caractère s_now  */
    strftime (s_now, sizeof s_now, "%d/%m/%Y %H:%M:%S", &tm_now);
-
-    FILE* f = NULL;
-
-    f=fopen("/home/watteau/Bureau/Projectss/history/stats.txt", "a"); // *
-
-    if (mode == 1)
-    {
-        if (f != NULL)
-        {
-            fprintf(f,"%s ;%d; %d.pbm\n", s_now, mode,*nbF);
-            fclose(f);
-        }
-    }
-    else if (mode == 2)
-    {
-        // Si 2, Taille d'affichage
-    }
-    else
-    {
+   
+   /* Ecrire la date, l'heure, le mode veille dans le fichier "stats.txt */
+   FILE* f = NULL;
+   
+   f=fopen("/home/watteau/Bureau/Projectss/history/stats.txt", "a"); // *
+   
+   if (mode == 1)
+   {
+      if (f != NULL)
+      {
+         fprintf(f,"%s ;%d; %d.pbm\n", s_now, mode,*nbF);
+         fclose(f);
+      }
+   }
+   else if (mode == 2)
+   {
+       // Si 2, Taille d'affichage
+   }
+   else
+   {
         // Si 3, position initiale avion sur la console
-    }
+   }
 }
 
